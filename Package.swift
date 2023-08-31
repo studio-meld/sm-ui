95dc57c61a7b82e685b2339588ca91bf70f47ecb
Fraser <fraserstirling@me.com>
2023-08-31 11:05:43 +1000

    Added iOS 15 or higher as a platform.

--- Package.swift
+++ Package.swift
@@ -1,23 +1,22 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "SMUI",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SMUI",
-            targets: ["SMUI"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SMUI"),
-        .testTarget(
-            name: "SMUITests",
-            dependencies: ["SMUI"]),
-    ]
+  name: "SMUI",
+  platforms: [
+    // Only add support for iOS 11 and up.
+    .iOS(.v15)
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(name: "SMUI", targets: ["SMUI"]),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(name: "SMUI"),
+    .testTarget(name: "SMUITests", dependencies: ["SMUI"]),
+  ]
 )
