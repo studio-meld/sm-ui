83a8e4dc0d21f2d933409f8c0e8051fe461fa8fd
fraser-0 <fraserstirling@me.com>
2023-11-08 10:26:03 +1100

    Updated access control

--- Sources/SMUI/ContainerView.swift
+++ Sources/SMUI/ContainerView.swift
@@ -1,11 +1,11 @@
 import SwiftUI
 
-protocol ContainerView: View {
+public protocol ContainerView: View {
   associatedtype Content
   init(content: @escaping () -> Content)
 }
 
-extension ContainerView {
+public extension ContainerView {
   init(@ViewBuilder _ content: @escaping () -> Content) {
     self.init(content: content)
   }
